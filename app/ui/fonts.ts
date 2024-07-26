071c3081be5843c9dcb177c4e4f3ef7cb1fe478b
Sergey <sayvix005@gmail.com>
2024-07-26 20:48:32 +0300

    initial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Rubik, Lusitana } from 'next/font/google';
+
+export const rubik =  Rubik({subsets: ["latin"]});
+export const lusitana = Lusitana({subsets: ["latin"], weight: ["700", "400"]})
